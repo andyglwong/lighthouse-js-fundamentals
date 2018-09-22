90e1db0796c514beb682a80b57c56ecb3c357a7c
Andy <andyglw87@gmail.com>
2018-09-22 08:50:47 +0000

    Adds functions exercises

---
+++ hello.js
@@ -0,0 +1,14 @@
+// function sayHello() {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+function sayHello(name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
